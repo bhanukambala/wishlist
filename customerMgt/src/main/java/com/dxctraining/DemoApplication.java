9dc5f07f8cd98f92cfc119876dcf4e6405a5dcbc
bhanukambala <kambalabhanu724@gmail.com>
2020-09-03 17:05:52 +0530

    changes

--- customerMgt/src/main/java/com/dxctraining/DemoApplication.java
+++ customerMgt/src/main/java/com/dxctraining/DemoApplication.java
@@ -2,19 +2,22 @@ package com.dxctraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
 	@Bean
 	public CorsFilter corsFilter(){
 		UrlBasedCorsConfigurationSource src=new UrlBasedCorsConfigurationSource();
@@ -27,10 +30,9 @@ public class DemoApplication {
 		return new CorsFilter(src);
 	}
 
-	
+	@LoadBalanced
 	@Bean
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
-
-}
+	}
